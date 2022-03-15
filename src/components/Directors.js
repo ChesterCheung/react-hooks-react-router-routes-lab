6c960ba818de3e7b9ffd50b703aa47d4cd813ee5
Chester <cheung319@gmail.com>
2022-03-14 23:18:17 -0400

    commit tree fc03bba25cfc4a6dc1bffa9501a6760ee2e293c6

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const directorsObj = directors.map((director) => (
+    <div key={director.name}>
+      {director.name}
+      <ul>{director.movies.map((movie) => (<li key={movie}>{movie}</li>))}</ul>
+    </div>
+  ))
+
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {directorsObj}
+  </div>)
 }
 
 export default Directors;
